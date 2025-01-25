09219b47ade5322386cb6baa7bd1660f490d933c
Alexander Tsioma <tsioma-aa@zvuk.com>
2025-01-25 23:18:13 +0500

    github actions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Calculator calculator = new Calculator();
+        int res = calculator.add(5,5 );
+
+        System.out.println("Result: " + res);
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(5,5 );
+        assertEquals(10, result);
+    }
 }
