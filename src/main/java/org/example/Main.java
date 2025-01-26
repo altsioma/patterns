a4416304018feab298572547209e9c3afaf3120e
Alexander Tsioma <tsioma-aa@zvuk.com>
2025-01-26 22:36:42 +0500

    github actions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,5 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int res = calculator.add(5,5 );
-
-        System.out.println("Result: " + res);
     }
 }
